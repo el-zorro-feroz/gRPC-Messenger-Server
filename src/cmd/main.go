17fe37c7c7df514ae2e65b1b1a3f38b942f70711
pocket-red-fox <pocket.red.fox@gmail.com>
2023-05-19 05:01:53 +0300

    feat(provider): invoke routes setup

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"log"
+	"main/src/api/routes"
 	"main/src/provider"
 
 	"github.com/joho/godotenv"
@@ -16,6 +17,7 @@ func RunServer() error {
 
 	app := fx.New(
 		provider.UsecaseModule,
+		fx.Invoke(routes.Setup),
 	)
 	app.Run()
 
