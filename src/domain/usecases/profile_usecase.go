ceba3b78a2f04c669b99ef6b869a37d549cc6612
pocket-red-fox <pocket.red.fox@gmail.com>
2023-05-19 04:16:36 +0300

    feat(server): update usecases

--- src/domain/usecases/profile_usecase.go
+++ src/domain/usecases/profile_usecase.go
@@ -1,1 +1,39 @@
 package usecase
+
+import (
+	"context"
+	"main/src/domain/entities"
+	domain "main/src/domain/repositories"
+	"time"
+)
+
+type ProfileUsecase interface {
+	GetProfileByID(c context.Context, userID string) (*entities.Profile, error)
+}
+
+type profileUsecase struct {
+	userRepository domain.UserRepository
+	contextTimeout time.Duration
+}
+
+func NewProfileUsecase(userRepository domain.UserRepository, timeout time.Duration) ProfileUsecase {
+	return &profileUsecase{
+		userRepository: userRepository,
+		contextTimeout: timeout,
+	}
+}
+
+func (pu *profileUsecase) GetProfileByID(c context.Context, userID string) (*entities.Profile, error) {
+	ctx, cancel := context.WithTimeout(c, pu.contextTimeout)
+	defer cancel()
+
+	user, err := pu.userRepository.GetByID(ctx, userID)
+	if err != nil {
+		return nil, err
+	}
+
+	return &entities.Profile{
+		Name:  user.Name,
+		Email: user.Email,
+	}, nil
+}
--- src/domain/usecases/refresh_token.go
+++ src/domain/usecases/refresh_token.go
@@ -1,1 +1,46 @@
 package usecase
+
+import (
+	"context"
+	"main/src/domain/entities"
+	domain "main/src/domain/repositories"
+	"main/src/utils"
+	"time"
+)
+
+type RefreshTokenUsecase interface {
+	GetUserByID(c context.Context, email string) (entities.User, error)
+	CreateAccessToken(user *entities.User, secret string, expiry int) (accessToken string, err error)
+	CreateRefreshToken(user *entities.User, secret string, expiry int) (refreshToken string, err error)
+	ExtractIDFromToken(requestToken string, secret string) (string, error)
+}
+
+type refreshTokenUsecase struct {
+	userRepository domain.UserRepository
+	contextTimeout time.Duration
+}
+
+func NewRefreshTokenUsecase(userRepository domain.UserRepository, timeout time.Duration) RefreshTokenUsecase {
+	return &refreshTokenUsecase{
+		userRepository: userRepository,
+		contextTimeout: timeout,
+	}
+}
+
+func (rtu *refreshTokenUsecase) GetUserByID(c context.Context, email string) (entities.User, error) {
+	ctx, cancel := context.WithTimeout(c, rtu.contextTimeout)
+	defer cancel()
+	return rtu.userRepository.GetByID(ctx, email)
+}
+
+func (rtu *refreshTokenUsecase) CreateAccessToken(user *entities.User, secret string, expiry int) (accessToken string, err error) {
+	return utils.CreateAccessToken(user, secret, expiry)
+}
+
+func (rtu *refreshTokenUsecase) CreateRefreshToken(user *entities.User, secret string, expiry int) (refreshToken string, err error) {
+	return utils.CreateRefreshToken(user, secret, expiry)
+}
+
+func (rtu *refreshTokenUsecase) ExtractIDFromToken(requestToken string, secret string) (string, error) {
+	return utils.ExtractIDFromToken(requestToken, secret)
+}
